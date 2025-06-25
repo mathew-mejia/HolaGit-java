36adeb7eed5eb0a749816e44c9f88a89143538c8
Mathew Mejia <mathies.mejia217@std.uni.edu.ni>
2025-06-25 16:00:00 -0600

    MENSAJE DE CONFIRMACION

---
+++ HolaGit.java
@@ -0,0 +1,14 @@
+public class HolaGit {
+    public static void main(String[] args) {
+
+    String nombre = "Mathew";
+    int a = 5;
+    int b = 3;
+    int suma = a + b;
+    
+    System.out.println("Hola, " + nombre + "!");
+    System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+
+
+    }
+    }
