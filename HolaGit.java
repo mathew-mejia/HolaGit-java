4fbc2875d756aa54cfb908c241d532d3afb13163
Mathew Mejia <mathies.mejia217@std.uni.edu.ni>
2025-06-25 16:19:49 -0600

    TERCER CAMBIO CONFIRMADO

--- HolaGit.java
+++ HolaGit.java
@@ -5,10 +5,11 @@ public class HolaGit {
     int a = 7;
     int b = 10;
     int suma = a + b;
+    int multiplicacion = a * b;
 
     System.out.println("Hola, " + nombre + "!");
     System.out.println("La suma de " + a + " + " + b + " es: " + suma);
-
+    System.out.println("La multiplicación de " + a + " + " + b + " es: " + multiplicacion);
 
     }
     }
