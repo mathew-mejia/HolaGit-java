3847379c120a6a6dad15766221c6f9fed979c1cd
Mathew Mejia <mathies.mejia217@std.uni.edu.ni>
2025-06-25 16:05:53 -0600

    SEGUNDA CONFIRMACION

--- HolaGit.java
+++ HolaGit.java
@@ -2,10 +2,10 @@ public class HolaGit {
     public static void main(String[] args) {
 
     String nombre = "Mathew";
-    int a = 5;
-    int b = 3;
+    int a = 7;
+    int b = 10;
     int suma = a + b;
-    
+
     System.out.println("Hola, " + nombre + "!");
     System.out.println("La suma de " + a + " + " + b + " es: " + suma);
 
